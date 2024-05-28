509da4383040828e3093f8348851f9670cdda949
Dudeiebot <ojoexcel5@gmail.com>
2024-05-28 13:07:10 +0100

    f: range through the lssps we have on python

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -21,8 +21,15 @@ lspconfig.gopls.setup {
   },
 }
 
-lspconfig.pyright.setup({
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = {"python"},
-})
+local servers = {
+  "pyright",
+  "ruff_lsp",
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = {"python"},
+  })
+end
