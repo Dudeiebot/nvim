fbe3f6109b8034595f2e283ab78a1c6e5edc6abb
Dudeiebot <ojoexcel5@gmail.com>
2024-07-11 17:24:24 +0100

    added prettier and eslint from none-ls

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -4,6 +4,8 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local opts = {
   sources = {
     null_ls.builtins.formatting.gofumpt,
+    require("none-ls.diagnostics.eslint_d"),
+    null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.goimports_reviser,
     null_ls.builtins.formatting.golines,
     null_ls.builtins.formatting.black,
