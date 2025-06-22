291bc9dc78e36536c31d3211b3d9eed32f764297
Dudeiebot <ojoexcel5@gmail.com>
2025-06-22 10:04:56 +0100

    feat: edit for ruff lsp

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -31,7 +31,7 @@ lspconfig.gopls.setup {
 
 local servers = {
   "pyright",
-  "ruff_lsp",
+  "ruff",
 }
 
 for _, lsp in ipairs(servers) do
@@ -43,7 +43,7 @@ for _, lsp in ipairs(servers) do
 end
 
 -- nvim-lsp config (:help lspconfig-all)
-lspconfig.tsserver.setup {
+lspconfig.ts_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
     cmd = { "typescript-language-server", "--stdio" },
