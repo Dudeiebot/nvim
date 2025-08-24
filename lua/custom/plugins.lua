26adbe8183740e1a24316c4a84179b11bc43a1dd
Dudeiebot <ojoexcel5@gmail.com>
2025-08-25 00:33:29 +0100

    feat: harpoon installation

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -196,5 +196,70 @@ local plugins = {
       vim.keymap.set('n', '<leader>tp', ':TypstPreview<CR>', { buffer = true, desc = 'Typst Preview' })
     end,
   },
+{
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { 
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup()
+
+  local conf = require("telescope.config").values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require("telescope.pickers").new({}, {
+        prompt_title = "Harpoon",
+        finder = require("telescope.finders").new_table({
+          results = file_paths,
+        }),
+        previewer = conf.file_previewer({}),
+        sorter = conf.generic_sorter({}),
+      }):find()
+    end
+
+    vim.keymap.set("n", "<leader>h", function() toggle_telescope(harpoon:list()) end,
+        { desc = "Open harpoon window" })
+  end,
+  keys = {
+    { "<leader>A", function() require("harpoon"):list():add() end, desc = "harpoon file" },
+    { "<leader>a", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu" },
+    { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1" },
+    { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2" },
+    { "<leader>3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3" },
+    { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4" },
+  },
+}
 }
 return plugins
+
+-- {
+--   "ThePrimeagen/harpoon",
+--   branch = "harpoon2",
+--   dependencies = { 
+--     "nvim-lua/plenary.nvim",
+--     "nvim-telescope/telescope.nvim",
+--   },
+--   config = function()
+--     local harpoon = require("harpoon")
+--     harpoon:setup()
+--
+--     -- Load the telescope extension
+--     require("telescope").load_extension("harpoon")
+--   end,
+--   keys = {
+--     { "<leader>A", function() require("harpoon"):list():append() end, desc = "harpoon file" },
+--     { "<leader>a", function() local harpoon = require("harpoon") harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = "harpoon quick menu" },
+--     { "<leader>h", ":Telescope harpoon marks<CR>", desc = "harpoon telescope" },
+--     { "<leader>1", function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1" },
+--     { "<leader>2", function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2" },
+--     { "<leader>3", function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3" },
+--     { "<leader>4", function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4" },
+--   },
+-- }
